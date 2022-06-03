70bf341af54502abafdae136d86f916acf041730
Cristian Sanchez Flores <andygign40@gmail.com>
2022-06-03 18:39:53 -0500

    Páginas portafolio y contacto

--- webPersonal/core/views.py
+++ webPersonal/core/views.py
@@ -5,6 +5,7 @@ html_base = """
     <ul>
         <li><a href="/">Portada</a></li>
         <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
     </ul>
 """
 
@@ -17,5 +18,16 @@ def home(request):
 def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
-        <p>Me llamo Héctor y me encanta Django!</p>
+        <p>Me llamo Cristian Sánchez Flores y estudio en la Universidad Tecnologica de Tlaxcala</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Mi correo Electronico y mis paginas web</p>
+        <ul>
+            <li><a href="mailto:cristian.san2908@gmail.com">Email</a></li>
+            <li><a href="https://github.com/Cristian-byte">Github</a></li>
+            <li><a href="https://youtu.be/o-YBDTqX_ZU">YouTube</a></li>
+        </ul>
     """)
